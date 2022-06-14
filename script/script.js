f95b33930aaa2a5e55525b9d0d19757f92916fad
Eduardo Santana <edusf0@hotmail.com>
2022-06-14 14:22:47 -0300

    finalizado primera parte

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let sliderElement = document.querySelector('#slider')
+let buttonElement = document.querySelector('#button')
+let sizePassoword = document.querySelector('#valor')
+let password = document.querySelector('#password')
+let containerPassword = document.querySelector('#container-password')
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+let novaSenha = ''
+
+sizePassoword.innerHTML = sliderElement.value
+slider.oninput = function () {
+  sizePassoword.innerHTML = this.value
+}
+
+function generationPassword() {
+  let pass = ''
+  for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
+    pass = pass + charset.charAt(Math.floor(Math.random() * n))
+  }
+
+  containerPassword.classList.remove('hide')
+  password.innerHTML = pass
+}
+
+function copiarSenha() {
+  navigator.clipboard
+    .writeText(password.innerHTML)
+    .then(() => {
+      console.log(password.innerHTML)
+    })
+    .catch(() => {
+      alert('something went wrong')
+    })
+}
